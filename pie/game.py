2f1ce002e08ce0322fecd84b9221d70362711c7b
hxwang51 <hxwang51@163.com>
2018-11-15 06:34:58 +0800

    game pie is commented

--- pie/game.py
+++ pie/game.py
@@ -6,6 +6,7 @@ from pygame.font import Font
 from piece import Piece
 
 def K_Index(index):
+    '''根据 pieces 中 piece 对象的索引 index 返回对应的 pygame 键值'''
     if index==0:
         return pygame.K_1
     elif index==1:
@@ -28,13 +29,27 @@ def K_Index(index):
         return pygame.K_9
 
 def draw_index(screen, index, centerx, centery, radius, startAngle, stopAngle, color):
-    font = Font(None, 30)
-    fontTextImage = font.render(str(index), True, color)
+    '''在 screen 上画出 pieces 中的 piece 对象的序号
+    screen: 将要画出序号的窗体
+    index:  将要画出的序号
+    centerx, centery: 饼图的圆心
+    radius:           饼图的半径
+    startAngle, stopAngle  每个 index 对应的 piece 对象的圆弧的起始弧度和结束弧度
+    color:  用来画出序号的颜色
+    '''
+    font = Font(None, 30) # None 表示为 Pygame 默认字体，字号为 30 号
+    fontTextImage = font.render(str(index), True, color)  # 把序号字符串渲染成图像
+    # 确定序号在窗口上的位置: x, y 坐标
     fontTextPosition = (centerx + (radius / 2) * math.cos((startAngle + stopAngle) / 2),
                         centery - (radius / 2) * math.sin((startAngle + stopAngle) / 2))
     screen.blit(fontTextImage, fontTextPosition)
 
 def get_start_stop_angles(index, numberPieces):
+    '''根据 piece 对象的序号和饼图划分的块数
+    确定 piece 上圆弧的起始弧度和结束弧度
+    startAngle: 起始弧度
+    stopAngle:  结束弧度
+    '''
     startAngle = math.radians(index * (360 / numberPieces))
     stopAngle = math.radians((index + 1) * (360 / numberPieces))
     return startAngle, stopAngle
@@ -49,11 +64,12 @@ def run_game():
     radius = 200
     pie_color = (200, 200, 0)
     bg_color = (0, 0, 200)
+    done_color = (230, 0, 0)
     lineWidth=5
 
-    numberPieces=9
+    numberPieces=9  # 取值范围为 1 到 9 ，因为最多只映射了 9 个数字按键
     pieces=[]
-    drawPieces = []
+    drawPieces = [] # Boolean 数组，用于标记是否已经画出每个 piece
     for index in range(numberPieces):
         startAngle, stopAngle = get_start_stop_angles(index, numberPieces)
         piece=Piece(screen, centerx, centery, radius, startAngle, stopAngle, lineWidth)
@@ -66,19 +82,19 @@ def run_game():
             if event.type == pygame.KEYDOWN:
                 for index in range(numberPieces):
                     if event.key == K_Index(index):
-                        drawPieces[index]=True
+                        drawPieces[index]=True   # 按下一个数字键就标记对应的 piece 被画出
         screen.fill(bg_color)
-        for index in range(numberPieces):
+        for index in range(numberPieces): # 画出序号
             startAngle, stopAngle = get_start_stop_angles(index, numberPieces)
             draw_index(screen, index+1, centerx, centery, radius, startAngle, stopAngle, pie_color)
-        allDrawed = True
+        allDrawed = True # 标记是否所有 piece 都已经被画出
         for index in range(numberPieces):
-            if drawPieces[index]:
+            if drawPieces[index]:  # 实际画出被标记为 True 的 piece
                 pieces[index].draw(pie_color)
             allDrawed = allDrawed and drawPieces[index]
-        if allDrawed:
+        if allDrawed: # 如果所有 piece 都已经被画出
             for piece in pieces:
-                piece.draw((230, 0, 0))
+                piece.draw(done_color)
         pygame.display.update()
 
 run_game()
--- pie/piece.py
+++ pie/piece.py
@@ -3,6 +3,17 @@ import math
 from pygame.font import Font
 
 class Piece():
+    '''这个类对饼图上的一小块进行抽象
+    __init__() 函数的参数:
+    screen:  主程序中由 pygame.display.set_mode() 创建的窗口对象
+    centerx, centery  饼图的圆心坐标
+                      ( screen 左上角的坐标为 (0, 0)，
+                        并且 y 坐标向下增长，
+                        x 坐标向右增长)
+    radius: 饼图的半径
+    startAngle, stopAngle Piece对象上的圆弧的起始和结束弧度数
+    lineWidth: 所绘制的线条的宽度
+    '''
     def __init__(self,
                  screen,
                  centerx, centery,
@@ -10,26 +21,23 @@ class Piece():
                  startAngle, stopAngle,
                  lineWidth):
         self.screen = screen
-        self.center = centerx, centery
+        self.center = centerx, centery   # 饼图的圆心 ( 坐标 )
         self.radius = radius
-        self.startAngle = startAngle
-        self.stopAngle = stopAngle
-        self.rect = self.getRect(self.center[0], self.center[1], radius, startAngle, stopAngle)
+        self.rect = (self.center[0]-self.radius, self.center[1]-radius,  # 饼图外切矩形的左上角x, y 坐标
+                     2*radius, 2*radius  # 饼图外切矩形的宽和高 width, height 当二者相等时绘制的是圆弧
+                    )                    # 当二者不相等时，绘制的是椭圆弧
+        self.startAngle = startAngle     # self.rect 的内切椭圆/圆上，圆心角为 startAngle 标记了圆弧的起始弧度
+        self.stopAngle = stopAngle       # self.rect 的内切椭圆/圆上，圆心角为 stopAngle 标记了圆弧的结束弧度
         self.lineWidth = lineWidth
 
-
-    def getRect(self, centerx, centery, radius, startAngle, stopAngle):
-        return centerx-radius, centery-radius, 2*radius, 2*radius
-
     def draw(self, color):
-        pygame.draw.arc(self.screen, color,self.rect,
+        # 画出 Piece 的圆弧弧线
+        pygame.draw.arc(self.screen, color, self.rect,
                         self.startAngle,self.stopAngle, self.lineWidth)
-        end1 = (self.center[0]+self.radius*math.cos(self.stopAngle),
+        end1 = (self.center[0]+self.radius*math.cos(self.stopAngle),  # 圆弧的一个端点的坐标
                 self.center[1]-self.radius*math.sin(self.stopAngle))
-        end2 = (self.center[0]+self.radius*math.cos(self.startAngle),
+        end2 = (self.center[0]+self.radius*math.cos(self.startAngle), # 圆弧的另一个端点的坐标
                 self.center[1]-self.radius*math.sin(self.startAngle))
+        # 从圆心到这两个端点绘制直线
         pygame.draw.line(self.screen, color, self.center, end1, self.lineWidth)
         pygame.draw.line(self.screen, color, self.center, end2, self.lineWidth)
-
-
-
